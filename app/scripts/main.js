a9a4d21a0812ce272e0d9c7f93d88f28f9303de8
harunhasdal <harun.hasdal@rma-consulting.com>
2015-04-19 10:21:12 +0100

    Add jshint validation.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,27 +1,25 @@
-var votingMap = new mapConvertor();
+'use strict';
+var electionAnalyser = function(d3, MapConvertor){
+	var votingMap = new MapConvertor(d3);
+	votingMap.initialise();
 
-votingMap.init();
+	var transform = function(e) {
+		return "translate(" + x(e.translate[0]) + "," + y(e.translate[1]) + ")scale(" + e.scale + ")";
+	};
 
-var svg = d3.select('svg');
-var width = svg.attr('width');
-var height = svg.attr('height');
+	var zoom = 	function() {
+		d3.selectAll('svg g').attr("transform", transform(d3.event));
+	};
 
+	var svg = d3.select('svg');
+	var width = svg.attr('width');
+	var height = svg.attr('height');
 
-var x = d3.scale.linear()
-	.domain([0, width]);
+	var x = d3.scale.linear().domain([0, width]);
+	var y = d3.scale.linear().domain([0, height]);
 
-var y = d3.scale.linear()
-	.domain([0, height]);
+	svg.call(d3.behavior.zoom().x(x).y(y).scaleExtent([1, 8]).on("zoom", zoom));
 
+};
 
-svg.call(d3.behavior.zoom().x(x).y(y).scaleExtent([1, 8]).on("zoom", zoom));
-
-
-function zoom() {
-	d3.selectAll('svg g').attr("transform", transform(d3.event));
-}
-
-function transform(e) {
-	return "translate(" + x(e.translate[0]) + "," + y(e.translate[1]) + ")scale(" + e.scale + ")";
-}
-
+electionAnalyser(window.d3, window.MapConvertor);
--- app/scripts/mapConvertor.js
+++ app/scripts/mapConvertor.js
@@ -1,7 +1,9 @@
-(function(){
+(function(d3){
+	'use strict';
+
 	var seats;
 
-	function mapConvertor() {
+	function MapConvertor() {
 
 		this.visibleParties = ["Con","Lab","LD"];
 
@@ -9,22 +11,22 @@
 		this.previousPercentageState = {};
 		this.elements = [];
 
-	};
+	}
 
-	mapConvertor.prototype.init = function(){
+	MapConvertor.prototype.initialise = function(){
 		this.loadData();
 		this.setTwentyTenResults();
 		this.updateRangeValues();
 	};
 
 
-	mapConvertor.prototype.setTwentyTenResults = function() {
+	MapConvertor.prototype.setTwentyTenResults = function() {
 		for (var party in this.twentyTenPercentageResult) {
 			this.previousPercentageState[party] = document.getElementById(party + '_rangeinput').value = this.twentyTenPercentageResult[party];
 		}
 	};
 
-	mapConvertor.prototype.recalculateSeats = function(node) {
+	MapConvertor.prototype.recalculateSeats = function(node) {
 
 		for (var party in this.twentyTenPercentageResult) {
 			this.elements[party + '_rangeinput'] = document.getElementById(party + '_rangeinput').value;
@@ -37,7 +39,7 @@
 
 	};
 
-	mapConvertor.prototype.updateVotes = function() {
+	MapConvertor.prototype.updateVotes = function() {
 		var voteDiffs = {};
 
 		for (var party in this.twentyTenPercentageResult) {
@@ -49,55 +51,54 @@
 		var otherParties = this.getOtherParties();
 
 		for (var i = 0; i < otherParties.length; i++) {
-			voteDiffs[otherParties[i]] = voteDiffs['other'];
+			voteDiffs[otherParties[i]] = voteDiffs.other;
 		}
 
-		delete voteDiffs['other']; // not a valid party anymore
+		delete voteDiffs.other; // not a valid party anymore
 
 		for (var id in seats){
 			var constituency = seats[id];
 			for (var voteDiffParty in voteDiffs) {
-				var adjustedVotes = Math.round( (constituency[voteDiffParty] * voteDiffs[voteDiffParty] ) );
-				constituency[voteDiffParty + '_adjusted'] = adjustedVotes;
+				constituency[voteDiffParty + '_adjusted'] = Math.round( (constituency[voteDiffParty] * voteDiffs[voteDiffParty] ) );
 			}
 			this.calculateSeatColor(constituency);
 		}
 	};
 
-	mapConvertor.prototype.getOtherParties = function() {
+	MapConvertor.prototype.getOtherParties = function() {
 		var allParties = ["AC","AD","AGS","APNI","APP","AWL","AWP","BB","BCP","Bean","Best","BGPV","BIB","BIC","Blue","BNP","BP Elvis","C28","Cam Soc","CG","Ch M","Ch P","CIP","CITY","CNPG","Comm","Comm L","Con","Cor D","CPA","CSP","CTDP","CURE","D Lab","D Nat","DDP","DUP","ED","EIP","EPA","FAWG","FDP","FFR","Grn","GSOT","Hum","ICHC","IEAC","IFED","ILEU","Impact","Ind1","Ind2","Ind3","Ind4","Ind5","IPT","ISGB","ISQM","IUK","IVH","IZB","JAC","Joy","JP","Lab","Land","LD","Lib","Libert","LIND","LLPB","LTT","MACI","MCP","MEDI","MEP","MIF","MK","MPEA","MRLP","MRP","Nat Lib","NCDV","ND","New","NF","NFP","NICF","Nobody","NSPS","PBP","PC","Pirate","PNDP","Poet","PPBF","PPE","PPNV","Reform","Respect","Rest","RRG","RTBP","SACL","Sci","SDLP","SEP","SF","SIG","SJP","SKGP","SMA","SMRA","SNP","Soc","Soc Alt","Soc Dem","Soc Lab","South","Speaker","SSP","TF","TOC","Trust","TUSC","TUV","UCUNF","UKIP","UPS","UV","VCCA","Vote","Wessex Reg","WRP","You","Youth","YRDPL"];
 		return allParties.filter(function(item){
 			return this.visibleParties.indexOf(item) === -1;
 		}.bind(this));
 	};
 
-	mapConvertor.prototype.reCalculateTo100percent = function(value_changed) {
-		value_changed = value_changed.replace(/\_.+/,'');
-		var amountChanged = this.elements[value_changed + '_rangeinput'] - this.previousPercentageState[value_changed];
+	MapConvertor.prototype.reCalculateTo100percent = function(valueChanged) {
+		valueChanged = valueChanged.replace(/\_.+/,'');
+		var amountChanged = this.elements[valueChanged + '_rangeinput'] - this.previousPercentageState[valueChanged];
 		amountChanged = amountChanged / 3; // 3 is the number of other other parties to share adjustment accross
 
 		// set the changed value to the state store
-		this.previousPercentageState[value_changed] = this.elements[value_changed + '_rangeinput'];
+		this.previousPercentageState[valueChanged] = this.elements[valueChanged + '_rangeinput'];
 
 		for (var party in this.twentyTenPercentageResult) {
-			if (value_changed !== party) {
+			if (valueChanged !== party) {
 				this.previousPercentageState[party] = this.previousPercentageState[party] - amountChanged;
-				document.getElementById(party + '_rangeinput').value = this.previousPercentageState[party]
+				document.getElementById(party + '_rangeinput').value = this.previousPercentageState[party];
 			}
 		}
 	};
 
-	mapConvertor.prototype.updateRangeValues = function() {
+	MapConvertor.prototype.updateRangeValues = function() {
 		for (var party in this.twentyTenPercentageResult) {
 			document.getElementById(party + '_rangevalue').value = Math.ceil(this.previousPercentageState[party] * 10) / 10;
 		}
 	};
 
-	mapConvertor.prototype.convertToId = function(constituencyName) {
+	MapConvertor.prototype.convertToId = function(constituencyName) {
 		return constituencyName.replace(/[\s,]/g,'_').replace(/&/g,'and').replace(/[()]/g,'');
 	};
 
-	mapConvertor.prototype.loadData = function() {
+	MapConvertor.prototype.loadData = function() {
 
 		d3.json("data/2010.json", function(constituencies) {
 			seats = constituencies;
@@ -108,7 +109,7 @@
 		}.bind(this));
 	};
 
-	mapConvertor.prototype.storeVotesPerConstituency = function(constituency) {
+	MapConvertor.prototype.storeVotesPerConstituency = function(constituency) {
 
 		// store in an adjusted variable as well
 		for (var party in constituency) {
@@ -119,12 +120,12 @@
 
 	};
 
-	mapConvertor.prototype.isValidParty = function(party) {
+	MapConvertor.prototype.isValidParty = function(party) {
 		var notValidParties = ['Press Association Reference','Constituency Name','Region','Election Year','Electorate','Votes'];
-		return !(notValidParties.indexOf(party) > -1);
+		return notValidParties.indexOf(party) === -1;
 	};
 
-	mapConvertor.prototype.calculateSeatColor = function(constituency) {
+	MapConvertor.prototype.calculateSeatColor = function(constituency) {
 		var winner = {party:null, votes:0};
 
 		var mapSeat = d3.select('#' + this.convertToId(constituency['Constituency Name']));
@@ -140,41 +141,38 @@
 		mapSeat.on('click',this.constituencyClickHandler);
 	};
 
-	mapConvertor.prototype.setSeatColor = function(ref, party) {
+	MapConvertor.prototype.setSeatColor = function(ref, party) {
 		var style = this.setStyle(party);
 		ref.attr('class', style + ' seat');
 	};
 
-	mapConvertor.prototype.partyCodeLookup = function(party) {
+	MapConvertor.prototype.partyCodeLookup = function(party) {
 		switch(party) {
 			case 'con':
 				return 'Con';
-				break;
 			case 'lab':
 				return 'Lab';
-				break;
 			case 'libdem':
 				return 'LD';
-				break;
 			default:
 				return 'other';
 		}
 	};
 
-	mapConvertor.prototype.setStyle = function(party) {
-		var styles = {'Lab':'labour','Con':'tory','LD':'libdem','SNP':'snp','Grn':'grn','PC':'pc','Respect':'respect','SDLP':'sdlp','PC':'pc','DUP':'dup','UUP':'uup','SF':'sf','UKIP':'ukip'};
+	MapConvertor.prototype.setStyle = function(party) {
+		var styles = {'Lab':'labour','Con':'tory','LD':'libdem','SNP':'snp','Grn':'grn','Respect':'respect','SDLP':'sdlp','PC':'pc','DUP':'dup','UUP':'uup','SF':'sf','UKIP':'ukip'};
 		return styles?styles[party] : 'unknown';
 	};
 
-	mapConvertor.prototype.constituencyClickHandler = function() {
+	MapConvertor.prototype.constituencyClickHandler = function() {
 		var id = this.getAttribute('id');
 
 		console.log(id + ': ' + JSON.stringify(seats[id]));
 	};
 
-	if(!window.mapConvertor){
-		window.mapConvertor = mapConvertor;
+	if(!window.MapConvertor){
+		window.MapConvertor = MapConvertor;
 	}
-	return mapConvertor;
+	return MapConvertor;
 })();
 
