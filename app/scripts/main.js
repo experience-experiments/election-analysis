f98efcc8cf970a2004c4a34b25248f8f404cd762
harunhasdal <harun.hasdal@rma-consulting.com>
2015-04-19 17:04:43 +0100

    adjust stroke-width on zoom

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,21 +5,24 @@ votingMap.initialise();
 
 (function mapZoom(){
 
-	var transform = function(e) {
-		return "translate(" + x(e.translate[0]) + "," + y(e.translate[1]) + ")scale(" + e.scale + ")";
-	};
-
-	var zoom = 	function() {
-		d3.selectAll('svg g').attr("transform", transform(d3.event));
-	};
-
 	var svg = d3.select('svg');
+	var allG = d3.selectAll('svg g');
+	var allPaths = d3.selectAll('svg path');
 	var width = svg.attr('width');
 	var height = svg.attr('height');
 
 	var x = d3.scale.linear().domain([0, width]);
 	var y = d3.scale.linear().domain([0, height]);
 
-	svg.call(d3.behavior.zoom().x(x).y(y).scaleExtent([1, 8]).on("zoom", zoom));
+	var transform = function(e) {
+		return "translate(" + x(e.translate[0]) + "," + y(e.translate[1]) + ")scale(" + e.scale + ")";
+	};
+
+	var zoom = 	function() {
+		allG.attr("transform", transform(d3.event));
+		allPaths.style("stroke-width", 0.5 / d3.event.scale);
+	};
+
+	svg.call(d3.behavior.zoom().x(x).y(y).scaleExtent([1, 9]).on("zoom", zoom));
 
 })();
